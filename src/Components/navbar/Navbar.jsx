962df0b8272f4c29de946ab8a44f486c27c3bf97
aaravmody <aaravmody2@gmail.com>
2024-05-01 01:48:41 +0530

    navbar

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const navbar = () => {
     <div className='navbar'>
     <Sidebar />
     <div className='wrapper'>
-        <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}> Aarav Mody</motion.span>
+        <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.51}}> Aarav Mody</motion.span>
         <div className='social'>
             <a href='#'><img src='/instagram.png' alt=''></img></a>
             <a href='#'><img src='/facebook.png' alt=''></img></a>
