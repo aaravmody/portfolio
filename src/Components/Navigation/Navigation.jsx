0d8a0a4b612bc3fbbb93722122552ec4445328e1
aaravmody <aaravmody2@gmail.com>
2024-05-01 13:01:26 +0530

    sidebar fix

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Sidebar from '../sidebar/Sidebar'
+import { motion } from 'framer-motion'
+import './Navigation.scss'
 
 const Navigation = () => {
   return (
     <div className='navbar'>
-    <Sidebar />
+    <Sidebar/>
     <div className='wrapper'>
         <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.51}}> Aarav Mody</motion.span>
         <div className='social'>
